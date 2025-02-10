a4820236c9ff447e35e52721417a3f73e99be84d
Luis Cobo <luis.cobo@gmail.com>
2025-02-10 15:26:43 -0500

    Update prueba01.py

--- taller03/prueba01.py
+++ taller03/prueba01.py
@@ -1,6 +1,5 @@
 # Ejemplo
 import __main__
-import importlib
 
 def assert_is_true(cond: bool) -> None:
     if cond:
@@ -9,7 +8,6 @@ def assert_is_true(cond: bool) -> None:
         print('FAIL')
 
 def prueba_sumar():
-    importlib.reload(__main__)
     print('Reloaded')
     m = __main__.sumar(3, 4)
     assert_is_true(7 == m)
